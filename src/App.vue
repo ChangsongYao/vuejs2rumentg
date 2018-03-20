2b2e1f48bd729e6dc8ea32329fa547de99b3b02f
Hidetada-komatu <2543144515@qq.com>
2018-03-20 22:06:25 +0800

    3.1数据驱动：从拼接到响应式计算

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
-  <div id="app">{{counter}}</div>
+  <div class="ez-clock">{{time | tformat('LTS')}}</div>
 </template>
 <script>
 
-export default {
-  name: 'App',
-  data:function () {
-    return{
-      counter:0
+  export default {
+    name: 'App',
+    data: function () {
+      return {
+        time: new Date()
+      }
+    },
+    mounted: function () {
+      setInterval(function () {
+        this.time = new Date();
+      }.bind(this), 1000)
+    },
+    filters: {
+      tformat: function (d, fmt) {
+        return moment(d).format(fmt);
+      }
     }
   }
-}
 
 
 </script>
 
 <style>
-
+  .ez-clock {
+    font-family: LED;
+    font-size: 60px;
+    background: #98b598;
+    padding: 20px;
+    padding-left: 100px;
+    display: inline-block;
+    letter-spacing: 10px;
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 1);
+  }
 </style>
