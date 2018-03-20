c7619db0dd5d3cd2b460255ee29f5a9894eb88f8
Hidetada-komatu <2543144515@qq.com>
2018-03-20 15:22:12 +0800

    1.1渐进式前端框架

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <div>
-    <h1>计算属性</h1>
-  </div>
+
 </template>
 
 <script>
 
-import EzRate from './components/EzRate'
-
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-  }
+  name: 'App'
 }
 
 
--- src/main.js
+++ src/main.js
@@ -1,22 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import EzRate from './components/EzRate'
-import 'font-awesome/css/font-awesome.css'   //是生效了的。
-
-
 
 Vue.config.productionTip = false
-
-
 /* eslint-disable no-new */
 
 new Vue({
-  el: '#app',
-  components:{
-    EzRate
-  },
-  template:'<EzRate/>'
+  el: '#app'
 })
 
 
