c0fcb714718e387c7bd7f87d98728b81f9959d9c
Hidetada-komatu <2543144515@qq.com>
2018-03-21 23:07:20 +0800

    10.4带作用域的槽

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,85 @@
 <template>
   <div id="app">
-    <ez-panel v-if="showPanel" title="CONTENT DISTRIBUTIOIN">
-      <button class="close" slot="header" @click="showPanel=false">x</button>
-      <img src="../static/img/carrot-2.jpg">
-    </ez-panel>
-    <h1 v-else>CLOSED!!</h1>
+    <ez-listview :data="goods">
+      <template scope="props">
+        <div class="item" :class="props.view">
+          <img :src="props.item.image">
+          <p>{{props.item.name}}</p>
+          <p>￥{{props.item.price}}</p>
+        </div>
+      </template>
+    </ez-listview>
   </div>
 </template>
 <script>
-  // import 'font-awesome/css/font-awesome.css'
-
-  import EzPanel from './components/EzPanel'
+  import EzListview from './components/EzListview'
 
   export default {
     name: 'App',
     data: function () {
       return {
-        showPanel:true
+        goods:[
+          {name:'双肩包男士多功能防盗背包 商务电脑包大学生书包女双肩背包',price:158,image:'../static/img/bag-1.jpg'},
+          {name:'休闲运动背包双肩包女书包中学生男时尚潮流大容量旅游旅行包',price:99,image:'../static/img/bag-2.jpg'},
+          {name:'防盗背包双肩包男士商务电脑包旅行包 大学生书包女双肩背包',price:158,image:'../static/img/bag-3.jpg'},
+          {name:'米熙双肩包女韩版潮 学院风校园印花背包男旅行包 中学生书包女包',price:99,image:'../static/img/bag-7.jpg'},
+        ]
       }
     },
-    components:{'ez-panel':EzPanel}
+    components:{'ez-listview':EzListview}
   }
 
 
 </script>
 
 <style>
-  #app .close{
-    float:right;
+  .clearfix:after{
+    display:block;
+    content:' ';
+    clear:both;
   }
-  .ez-panel{
-    border: 1px solid #222;
+  #app div.item.small{
+    display:flex;
+  }
+  #app div.item.small> p:nth-child(2){
+    flex:1;
+  }
+  #app div.item.small> p:last-child{
+    width:100px;
+  }
+
+  .ez-listview{
+    border:1px solid #222;
     border-radius:5px;
   }
-  .ez-panel .header{
-    background:blue;
-    color:white;
-    padding: 5px;
-  }
-  .ez-panel .content{
+  .ez-listview .header{
+    background:#f0f0f0;
     padding:5px;
   }
-  .ez-panel .content:after{
-    content:' ';
+  .ez-listview .toolbar{
+    word-spacing:-10px;
+    float:left;
+  }
+  .ez-listview .content{
+    padding:5px;
+  }
+  .ez-listview ul{
+    list-style:none;
+    padding:0;
+    margin:0;
+  }
+  ul li{
     display:block;
-    clear:both;
+    border:1px solid #ccc;
+  }
+  ul.large li{
+    float:left;
+    width:200px;
+    height:350px;
+    padding:5px;
+    box-sizing:border-box;
+  }
+  ul.large li img{
+    width:100%;
   }
 </style>
---
+++ src/components/EzListview.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="ez-listview">
+    <div class="header clearfix">
+      <div class="toolbar">
+        <button>综合排序</button>
+        <button>销量 &darr;</button>
+        <button>价格 &darr;</button>
+        <button>口碑 &darr;</button>
+      </div>
+      <div class="toolbar">
+        <button @click="viewType='large'"><i class="fa fa-th-large" @click="viewType='large'"></i> 大图</button>
+        <button @click="viewType='small'"><i class="fa fa-list" @click="viewType='small'"></i> 小图</button>
+      </div>
+    </div>
+    <div class="content">
+      <ul :class="viewType" class="clearfix">
+        <li v-for="d in data">
+          <slot :item="d" :view="viewType">{{d.name}}</slot>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ez-listview",
+    props: ['data'],
+    data: function () {
+      return {
+        viewType: 'large'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
