d69a2bbdddd9912bee70398a8d3ca3e031816cb4
Hidetada-komatu <2543144515@qq.com>
2018-03-21 22:52:21 +0800

    10.1概述

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,40 @@
 <template>
-  <div id="app" tabindex="0" @keydown.down="v--"  @keydown.up="v++">
-    <button @click="v--"> &darr; 刹车</button>
-    <label class="v">{{v}}</label>
-    <button @click="v++"> &uarr; 油门</button>
-    <span class="status">{{status}}</span>
-    <ez-gauge :value.sync="v" @overload="onOverload"></ez-gauge>
+  <div id="app">
+    <ez-panel title="SLOT IS A PLACEHOLDER">
+      <img src="../static/img/slot-sketch.jpg">
+    </ez-panel>
   </div>
 </template>
 <script>
   // import 'font-awesome/css/font-awesome.css'
 
-  import EzGauge from './components/EzGauge'
+  import EzPanel from './components/EzPanel'
 
   export default {
     name: 'App',
     data: function () {
       return {
-        v:78,
-        status:''
+
       }
     },
-    methods:{
-      onOverload:function(flag){
-        if(flag) this.status = 'OVERLOAD!!!';
-        else this.status = '';
-      }
-    },
-    components:{
-      'ez-gauge': EzGauge
-    }
+    components:{'ez-panel':EzPanel}
   }
 
 
 </script>
 
 <style>
-  body{
-    background:#222;
+  .ez-panel{
+    border: 1px solid #222;
+    border-radius:5px;
   }
-  #app{
-    outline:none;
-  }
-  .ez-gauge {
-    width: 400px;
-    height: 200px;
-    position: absolute;
-    top: 150px;
-    left: 200px;
-    overflow: hidden;
-    text-align: center;
-    transform: translate(-50%, -50%);
-  }
-
-  .outer {
-    z-index: 1;
-    position: absolute;
-    background-color: rgba(255,255,255,.2);
-    width: 400px;
-    height: 200px;
-    top: 0%;
-    border-radius: 250px 250px 0px 0px;
-    cursor:pointer;
-  }
-
-  .inner {
-    z-index: 3;
-    position: absolute;
-    background-color: #222;
-    width: 250px;
-    height: 125px;
-    top: 75px;
-    margin-left: 75px;
-    margin-right: auto;
-    border-radius: 250px 250px 0px 0px;
-    pointer-events:none;
-  }
-
-  .indicator {
-    z-index: 2;
-    position: absolute;
-    background-color: #4fda10;
-    width: 400px;
-    height: 200px;
-    top: 200px;
-    margin-left: auto;
-    margin-right: auto;
-    border-radius: 0px 0px 200px 200px;
-    transform-origin: center top;
-    transition: all .1s ease-in-out;
-    pointer-events:none;
-  }
-
-  .indicator.overload{
-    background-color:red;
-  }
-
-  .ez-gauge:hover .indicator {  transform:rotate(.5turn);
-  }
-
-  .ez-gauge:hover .data { color: rgba(255,255,255,1); }
-
-  .data {
-    z-index: 4;
-    color: rgba(255,255,255,.2);
-    font-size: 1.2em;
-    line-height: 25px;
-    position: absolute;
-    width: 400px;
-    height: 200px;
-    top: 90px;
-    margin-left: auto;
-    margin-right: auto;
-    transition: all 1s ease-out;
-    pointer-events:none;
-  }
-  .status{
-    color: red;
-  }
-  .v{
+  .ez-panel .header{
+    background:blue;
     color:white;
+    padding: 5px;
   }
+  .ez-panel .content{
+    padding:5px;
+  }
+
 </style>
---
+++ src/components/EzPanel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ez-panel">
+    <div class="header">{{title}}</div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ez-panel",
+    props: {
+      title: {default: 'Panel Title', type: String}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
