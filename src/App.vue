fd2474a111d813af511845c5d17c98dbfb68c38f
Hidetada-komatu <2543144515@qq.com>
2018-03-21 23:59:40 +0800

    12.3使用transition组件

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,34 @@
 <template>
   <div id="app">
-    <div>FRAME: {{idx}}</div>
-    <div class="sprite" :style="{'background-position':pos}"></div>
+    <button @click="flag=!flag">toggle</button>
+    <transition>
+      <div v-show="flag">
+        <img src="../static/img/scene-3.jpg">
+      </div>
+    </transition>
   </div>
 </template>
 <script>
-  import 'font-awesome/css/font-awesome.css'
   export default {
     name: 'App',
     data: function () {
       return {
-        idx: 0,
-        w:document.body.clientWidth
+        flag: true
       }
-    },
-    computed:{
-      pos:function(){
-        var sw = 165,sh = 292;
-        var c = this.idx % 12;
-        var r = Math.floor(this.idx / 12);
-        var x = -sw * c;
-        var y = -sh * r;
-        return x + 'px ' + y + 'px';
-      }
-    },
-    mounted:function(){
-      requestAnimationFrame(function step(){
-        this.idx = (this.idx +1) % 64;
-        requestAnimationFrame(step);
-      })
     }
   }
 </script>
 
 <style>
-  body{
-    background:url(../static/img/sky.png) repeat-x;
+  img {
+    width: 100%;
   }
-  .sprite{
-    background:url(../static/img/spritesheet_grant.png);
-    width:165px;
-    height:292px;
+
+  .v-enter-active, .v-leave-active {
+    transition: all 1s
+  }
+
+  .v-enter, .v-leave-to {
+    opacity: 0
   }
 </style>
