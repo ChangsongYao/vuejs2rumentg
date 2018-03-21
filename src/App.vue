2643948265d2db2af39b1e3d24bd5631a9787e16
Hidetada-komatu <2543144515@qq.com>
2018-03-21 17:48:24 +0800

    9.5组件的属性 在单独的页面中仪表盘的值不会动 <!doctype html> <html> <head>   <meta charset="utf-8"> </head> <body>
      <div id="app">
        <ez-gauge min="0" max="100" :value="v"></ez-gauge>
        <input v-model.number="v" type="number">
      </div>
    
      <template id="tpl-gauge">
        <div class="ez-gauge">
          <div class="outer"></div>
          <div class="inner"></div>
          <div class="indicator" :style="rotateStyle"></div>
          <div class="data">
            <h1>{{value}}</h1>
          </div>
        </div>
      </template>
    
      <script src="lib/vue.js"></script>
    
      <script>
        var EzGauge = {
          template: '#tpl-gauge',
          props: ['value','min','max'],
          computed:{
            rotateStyle:function(){
              var val = this.value < this.min ? 0 : (this.value > this.max ? this.max: this.value);
              var angle = val * 180/(this.max-this.min);
              return {transform:'rotate(' + angle+'deg)'}
            }
          }
        };
    
        new Vue({
          el: '#app',
          data:{v:34},
          components:{
            'ez-gauge': EzGauge
          }
        });
    
      </script>
    </body>
    </html>

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app">
-    <h1 v-dumb>哑指令</h1>
+    <ez-gauge min="0" max="100" :value="v"></ez-gauge>
+    <input v-model.number="v" type="number">
   </div>
 </template>
 <script>
   // import 'font-awesome/css/font-awesome.css'
 
+  import EzGauge from './components/EzGauge'
+
   export default {
     name: 'App',
-    // data: function () {
-    //   return {
-    //     gender: '',
-    //     nations:[]
-    //   }
-    // }
-    directives:{
-      'dumb':{}
+    data: function () {
+      return {
+        v: 34
+      }
+    },
+    components: {
+      'ez-gauge': EzGauge
     }
   }
 
@@ -23,8 +25,72 @@
 </script>
 
 <style>
-  select{
-    min-width:150px;
+  body{
+    background:#222;
+  }
+  .ez-gauge {
+    width: 400px;
+    height: 200px;
+    position: absolute;
+    top: 150px;
+    left: 200px;
+    overflow: hidden;
+    text-align: center;
+    transform: translate(-50%, -50%);
   }
 
+  .outer {
+    z-index: 1;
+    position: absolute;
+    background-color: rgba(255,255,255,.2);
+    width: 400px;
+    height: 200px;
+    top: 0%;
+    border-radius: 250px 250px 0px 0px;
+  }
+
+  .inner {
+    z-index: 3;
+    position: absolute;
+    background-color: #222;
+    width: 250px;
+    height: 125px;
+    top: 75px;
+    margin-left: 75px;
+    margin-right: auto;
+    border-radius: 250px 250px 0px 0px;
+  }
+
+  .indicator {
+    z-index: 2;
+    position: absolute;
+    background-color: #5664F9;
+    width: 400px;
+    height: 200px;
+    top: 200px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 0px 0px 200px 200px;
+    transform-origin: center top;
+    transition: all .5s ease-in-out;
+  }
+
+  .ez-gauge:hover .indicator {  transform:rotate(.5turn);
+  }
+
+  .ez-gauge:hover .data { color: rgba(255,255,255,1); }
+
+  .data {
+    z-index: 4;
+    color: rgba(255,255,255,.2);
+    font-size: 1.5em;
+    line-height: 25px;
+    position: absolute;
+    width: 400px;
+    height: 200px;
+    top: 90px;
+    margin-left: auto;
+    margin-right: auto;
+    transition: all 1s ease-out;
+  }
 </style>
---
+++ src/components/EzGauge.vue
@@ -0,0 +1,28 @@
+<template id="tpl-gauge">
+  <div class="ez-gauge">
+    <div class="outer"></div>
+    <div class="inner"></div>
+    <div class="indicator" :style="rotateStyle"></div>
+    <div class="data">
+      <h1>{{value}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ez-gauge",
+        props: ['value','min','max'],
+        computed:{
+          rotateStyle:function(){
+            var val = this.value < this.min ? 0 : (this.value > this.max ? this.max: this.value);
+            var angle = val * 180/(this.max-this.min);
+            return {transform:'rotate(' + angle+'deg)'}
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
