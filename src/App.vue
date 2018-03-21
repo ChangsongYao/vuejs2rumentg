86f32264acc2fdca8b04e88085370d32ceea5e27
Hidetada-komatu <2543144515@qq.com>
2018-03-21 16:09:43 +0800

    7.6绑定选择列表输入

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
-    <h4>绑定单选框组</h4>
-    <div v-for="item in items">
-      <input type="radio"  :id="item" :value="item" v-model="selected">
-      <label :for="item">{{item}}</label>
+    <div class="single">
+      <h4>绑定单选列表</h4>
+      <select v-model="gender">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+      <p>gender: {{gender}}</p>
     </div>
-    <p>selected: {{selected}}</p>
+    <div class="multiple">
+      <h4>绑定多选列表</h4>
+      <select multiple v-model="nations">
+        <option value="CN">China</option>
+        <option value="JPN">Japan</option>
+        <option value="KR">Korea</option>
+      </select>
+      <p>nations:{{nations}}</p>
+    </div>
   </div>
 </template>
 <script>
-  import 'font-awesome/css/font-awesome.css'
+  // import 'font-awesome/css/font-awesome.css'
 
   export default {
     name: 'App',
     data: function () {
       return {
-        items: ['China','Japan','Korea'],
-        selected: ''
+        gender: '',
+        nations:[]
       }
     }
   }
@@ -25,4 +36,8 @@
 </script>
 
 <style>
+  select{
+    min-width:150px;
+  }
+
 </style>
