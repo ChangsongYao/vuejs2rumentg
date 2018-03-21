fc218f4c65adf69157ac9dc2968a00ab969ef436
Hidetada-komatu <2543144515@qq.com>
2018-03-21 13:41:14 +0800

    4.5样式绑定语法

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
-    <h4>.prop</h4>
-    <p :text-content.prop="content"></p>
+    <h4>
+      style属性绑定
+      <button @click="fly"> 让我飞！</button>
+    </h4>
+    <img class="bird" src="../static/img/bird.jpg" :style="pos">
   </div>
 </template>
 <script>
+  // let w = document.body.clientWidth,
+  //   h = document.body.clientHeight;
 
   export default {
     name: 'App',
     data: function () {
       return {
-        content: '使用<b>.prop</b>修饰符绑定DOM属性'
+        x:0,y:200,
+        w:document.body.clientWidth,
+        h:document.body.clientHeight
+      }
+    },
+    computed:{
+      pos:function(){
+        return {left:this.x+'px',top:this.y+'px'}
+      }
+    },
+    methods:{
+      fly:function(){
+        this.x = Math.floor(Math.random()*this.w);
+        this.y = Math.floor(Math.random()*this.h);
       }
     }
   }
@@ -19,4 +37,13 @@
 </script>
 
 <style>
+  html,body{
+    height:100%;
+    overflow:hidden;
+  }
+  img.bird{
+    position:absolute;
+    width:100px;
+    transition:all 1s;
+  }
 </style>
