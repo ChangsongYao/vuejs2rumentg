25f71d117d9678bd0dea8952a2e82e25ce1ff15a
Hidetada-komatu <2543144515@qq.com>
2018-03-21 16:08:47 +0800

    7.5绑定单选框输入

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <template>
   <div id="app">
-    <div class="toolbar">
-      <input type="checkbox" id="nightMode">
-      <label for="toggle">夜晚阅读模式</label>
-    </div>
-
-    <h4>绑定单个复选框</h4>
-    <input id="remember" type="checkbox" v-model="checked">
-    <label for="remember">remember me!</label>
-    <p>rememberme: {{checked}}</p>
-
-    <h4>绑定复选框组</h4>
+    <h4>绑定单选框组</h4>
     <div v-for="item in items">
-      <input type="checkbox"  :id="item" :value="item" v-model="selected">
+      <input type="radio"  :id="item" :value="item" v-model="selected">
       <label :for="item">{{item}}</label>
     </div>
     <p>selected: {{selected}}</p>
@@ -25,9 +15,8 @@
     name: 'App',
     data: function () {
       return {
-        checked: false,
         items: ['China','Japan','Korea'],
-        selected: []
+        selected: ''
       }
     }
   }
@@ -36,9 +25,4 @@
 </script>
 
 <style>
-  div.toolbar{
-    background:#777;
-    color:yellow;
-    padding:6px;
-  }
 </style>
