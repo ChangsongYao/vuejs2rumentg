68a7eac2658de1a852c9c01417621fedb13f4dce
Hidetada-komatu <2543144515@qq.com>
2018-03-22 00:13:32 +0800

    bug12.8集成第三方JS动画库 <div id="app">     <div>       <p>         <button @click="flag=!flag">切换 显示/隐藏</button>       </p>       <transition name="demo"         @beforeEnter="beforeEnter"         @enter="enter"         @leave="leave"         :css="false">         <img v-show="flag" src="img/scene-3.jpg" class="target">       </transition>     </div>   </div>
      <script src="lib/vue.js"></script>
      <script src="lib/velocity.js"></script>
    
      <script>
        new Vue({
          el:'#app',
          data:{flag:true},
          methods:{
            beforeEnter:function(){
        el.style.opacity = 0
              el.style.transformOrigin = 'left';
              el.style.rotateZ='0deg';
            },
            enter: function (el, done) {
              Velocity(el, {
                rotateZ:'0deg',
               opacity: 1
                }, { duration: 300,complete:done })
            },
            leave: function (el, done) {
              Velocity(el, {
                rotateZ: '45deg',
                opacity: 0
              }, { complete: done })
            }
          }
        });
      </script>

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
         <button @click="flag=!flag">切换 显示/隐藏</button>
       </p>
       <transition name="demo"
+                  @beforeEnter="beforeEnter"
                   @enter="enter"
                   @leave="leave"
                   :css="false">
@@ -21,31 +22,29 @@
         flag: true
       }
     },
-    methods: {
+    methods:{
+      beforeEnter:function(){
+        el.style.opacity = 0
+        el.style.transformOrigin = 'left';
+        el.style.rotateZ='0deg';
+      },
       enter: function (el, done) {
-        var step = function () {
-          var style = getComputedStyle(el);
-          el.style.opacity = +style.opacity + 0.05;
-          if (+el.style.opacity < 1) requestAnimationFrame(step);
-          else done();
-        };
-        requestAnimationFrame(step);
+        Velocity(el, {
+          rotateZ:'0deg',
+          opacity: 1
+        }, { duration: 300,complete:done })
       },
       leave: function (el, done) {
-        var step = function () {
-          var style = getComputedStyle(el);
-          el.style.opacity = +style.opacity - 0.05;
-          if (+el.style.opacity > 0) requestAnimationFrame(step);
-          else done();
-        };
-        requestAnimationFrame(step);
+        Velocity(el, {
+          rotateZ: '45deg',
+          opacity: 0
+        }, { complete: done })
       }
     }
   }
 </script>
 
 <style>
-  body{overflow:hidden}
-  img{width:100%;}
-
+  body{overflow:hidden;}
+  img{width:100%}
 </style>
