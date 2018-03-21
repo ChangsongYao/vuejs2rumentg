d66c8449f0390d605fbb2b6130be199b18a51817
Hidetada-komatu <2543144515@qq.com>
2018-03-21 13:35:51 +0800

    4.4DOM属性绑定

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img :src="images[idx]">
-    <a href="#" target="_blank">在大窗查看</a>
+    <h4>.prop</h4>
+    <p :text-content.prop="content"></p>
   </div>
 </template>
 <script>
@@ -10,18 +10,8 @@
     name: 'App',
     data: function () {
       return {
-        images:[
-          'static/img/scene-1.jpg',
-          'static/img/scene-2.jpg',
-          'static/img/scene-3.jpg'
-        ],
-        idx:0
+        content: '使用<b>.prop</b>修饰符绑定DOM属性'
       }
-    },
-    created:function(){
-      setInterval(function(){
-        this.idx = (this.idx+1) % this.images.length
-      }.bind(this),2000);
     }
   }
 
@@ -29,24 +19,4 @@
 </script>
 
 <style>
-  #app{
-    position:absolute;
-    left:0;
-    top:0;
-    right:0;
-    bottom:0;
-  }
-  #app img{
-    width:100%;
-    height:100%;
-  }
-  #app a{
-    position:absolute;
-    right:10px;
-    top:10px;
-    background:#695252;
-    color:#fff;
-    padding:10px 20px;
-    text-decoration:none;
-  }
 </style>
