edd3d225ee9f0c0075e22dcaa7ec1586706732fa
Hidetada-komatu <2543144515@qq.com>
2018-03-21 23:49:11 +0800

    12.1CCS过渡和动画

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,59 @@
 <template>
   <div id="app">
-    <h4>评级组件</h4>
-    <ez-rate></ez-rate>
+    <img src="../static/img/the-road-to-ordianry.jpg" :class="{playing:playing}">
+    <span :class="{fa:true,'fa-play-circle':!playing,'fa-stop-circle':playing}"
+          @click="playing=!playing" class="control"></span>
   </div>
 </template>
 <script>
   import 'font-awesome/css/font-awesome.css'
-  import EzRate from './components/EzRate'
 
   export default {
     name: 'App',
     data: function () {
-      return {}
-    },
-    components: {
-      'ez-rate': EzRate
+      return {
+        playing: false
+      }
     }
   }
 </script>
 
 <style>
+  #app {
+    transition: all .2s;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    overflow: hidden;
+    position: relative;
+  }
+
+  span.fa {
+    position: absolute;
+    font-size: 50px;
+    left: 78px;
+    top: 75px;
+    color: #333;
+    cursor: pointer;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  img:hover {
+  }
+
+  .playing {
+    animation: turn 30s linear infinite;
+  }
+
+  @keyframes turn {
+    0% {
+      transform: rotate(0deg)
+    }
+    100% {
+      transform: rotate(360deg)
+    }
+  }
 </style>
