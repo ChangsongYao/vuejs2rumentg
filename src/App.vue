a6f6a385666669020aaf012c201f2e88d4dee540
Hidetada-komatu <2543144515@qq.com>
2018-03-21 16:06:45 +0800

    7.3绑定多行文本输入

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,7 @@
 <template>
   <div id="app">
-    <form>
-      <div class="lazy">
-        <h4>.lazy</h4>
-        <p><input v-model="query" placeholder="without .lazy">{{query}}</p>
-        <p><input v-model.lazy="queryLazy" placeholder="with .lazy">{{queryLazy}}</p>
-      </div>
-      <div class="trim">
-        <h4>.trim</h4>
-        <p>
-          <input v-model="title" placeholder="without .trim">
-          {{title.replace(/\s/g,'_')}} / {{title.length}}
-        </p>
-        <p>
-          <input v-model.trim="titleTrim" placeholder="with .trim">
-          {{titleTrim.replace(/\s/g,'_')}} / {{titleTrim.length}}
-        </p>
-      </div>
-      <div class="number">
-        <h4>.number</h4>
-        <p>
-          <input type="number" v-model="quantity" placeholder="quantity">
-          {{quantity}} / {{typeof quantity}}
-        </p>
-        <p>
-          <input type="number" v-model.number="quantityNumber" placeholder="with .number">
-          {{quantityNumber}} / {{typeof quantityNumber}}
-        </p>
-      </div>
-    </form>
+    <textarea v-model="raw"></textarea>
+    <div class="canvas" v-html="parsed"></div>
   </div>
 </template>
 <script>
@@ -38,12 +11,12 @@
     name: 'App',
     data: function () {
       return {
-        query:'',
-        queryLazy:'',
-        title:'',
-        titleTrim:'',
-        quantity:'10',
-        quantityNumber:10
+        raw: '# demo\n\nthis is a paragraph.and following is a list.\n\n- item 1\n- item 2'
+      }
+    },
+    computed:{
+      parsed: function(){
+        return marked(this.raw);
       }
     }
   }
@@ -52,6 +25,23 @@
 </script>
 
 <style>
-  html,body,#app,.protected{height:100%;}
-  pre{display:inline-block}
+  html,body,#app{
+    height: 100%;
+  }
+  #app{
+    display:flex;
+  }
+  #app > *{
+    flex:1;
+  }
+  #app textarea{
+    font-family:Consolas;
+    outline:none;
+    border:0;
+    border-right:1px solid black;
+  }
+  #app div{
+    padding:0 10px;
+  }
+
 </style>
