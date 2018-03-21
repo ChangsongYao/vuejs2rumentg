c7fa53baf2ba51b5c611a9af77cb3dc5b5346c25
Hidetada-komatu <2543144515@qq.com>
2018-03-21 16:12:54 +0800

    8.2创建自定义指令

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <div id="app">
-    <div class="single">
-      <h4>绑定单选列表</h4>
-      <select v-model="gender">
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-      </select>
-      <p>gender: {{gender}}</p>
-    </div>
-    <div class="multiple">
-      <h4>绑定多选列表</h4>
-      <select multiple v-model="nations">
-        <option value="CN">China</option>
-        <option value="JPN">Japan</option>
-        <option value="KR">Korea</option>
-      </select>
-      <p>nations:{{nations}}</p>
-    </div>
+    <h1 v-dumb>哑指令</h1>
   </div>
 </template>
 <script>
@@ -24,11 +8,14 @@
 
   export default {
     name: 'App',
-    data: function () {
-      return {
-        gender: '',
-        nations:[]
-      }
+    // data: function () {
+    //   return {
+    //     gender: '',
+    //     nations:[]
+    //   }
+    // }
+    directives:{
+      'dumb':{}
     }
   }
 
